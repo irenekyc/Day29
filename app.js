5dff7835d41616ce0318fed5182447d67e125bc5
irenekyc <gugu_irene@hotmail.com>
2020-03-21 10:49:19 -0500

    change percentage value

--- app.js
+++ app.js
@@ -1,45 +1,49 @@
+// DOM clarification for each items that need to be reveal
 const portfolioCard = document.querySelector('.portfolio-cards-container')
 const qcardcontainer = document.querySelector('.qualification-cards')
 const qcards = document.querySelectorAll('.qualification-card')
 const aboutmeItems = document.querySelectorAll('.title-to-be-revealed')
 const aboutmeContainer = document.querySelector('.about-hero-container')
 const quoteContainer = document.querySelector('.quote-to-be-revealed')
-let previousScrollY = window.scrollY
 const windowHeight = window.innerHeight
-let scrollDirection , scrollPercentage
+let scrollPercentage
 
+// add event listener on scroll
 
-//determin scrolling direction
 window.addEventListener('scroll', ()=>{
-    // if (window.scrollY>previousScrollY){
-    //     scrollDirection = 'down'
-        
-    // } else (
-    //     scrollDirection = 'up'
-    // )
-    // console.log(scrollDirection)
-    // previousScrollY = window.scrollY;
+//calculate the elements items in relation to the window broswer height
     if (window.scrollY + windowHeight > portfolioCard.offsetTop){
+
+// if the item reach the bottom of window browser , add css class to reveal item 
         scrollPercentage = (portfolioCard.getBoundingClientRect().y / windowHeight)*100
-        if (scrollPercentage > 99){
+        if (scrollPercentage < 70){
         portfolioCard.classList.add('is-reveal')
         }
     }
     if (window.scrollY + windowHeight > qcardcontainer.offsetTop){
         scrollPercentage = (qcardcontainer.getBoundingClientRect().y / windowHeight)*100
-        if (scrollPercentage > 99){
+        if (scrollPercentage < 50){
         qcards.forEach((card)=>{
             card.classList.add('card-reveal')
         })}}
     
     if (window.scrollY + windowHeight > aboutmeContainer.offsetTop){
         scrollPercentage = (aboutmeContainer.getBoundingClientRect().y / windowHeight)*100
-        if (scrollPercentage > 95){
+        if (scrollPercentage < 50){
         aboutmeItems.forEach((item)=>{
             item.classList.add('fadeFromLeft')
         })
         quoteContainer.classList.add('fadeFromRight')}
     }
     })
+
+
+
+
+
+
+
+
+
 
 
